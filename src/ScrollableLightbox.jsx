c60309e8ee2c1ee8745f520f26a5559de3cea1d6
hfchou <tim_chou@chowsangsang.com>
2019-11-19 15:27:56 +0800

    down up

--- src/ScrollableLightbox.jsx
+++ src/ScrollableLightbox.jsx
@@ -4,18 +4,15 @@ export default class ScrollableLightbox extends Component {
   render() {
     const { show, close, title } = this.props;
     return (
-      (show && (
-        <div className="lightbox">
-          <div className="content">
-            <button className="lightbox-close-btn" onClick={close}></button>
-            <div className="lightbox-header">
-              <div className="lightbox-title">{title}</div>
-            </div>
-            <div className="lightbox-body">{this.props.children}</div>
+      <div className={show ? "lightbox" : "lightbox lightbox-close"}>
+        <div className={show ? "content down" : "content up"}>
+          <button className="lightbox-close-btn" onClick={close}></button>
+          <div className="lightbox-header">
+            <div className="lightbox-title">{title}</div>
           </div>
+          <div className="lightbox-body">{this.props.children}</div>
         </div>
-      )) ||
-      null
+      </div>
     );
   }
 }
